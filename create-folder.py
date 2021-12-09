0208da64eae5cc30d9c8f43de4c8e40c3b64ac2b
Nigel Dias <d_nigel_@hotmail.com>
2021-12-08 21:19:38 -0500

    Update create-folder.py

--- create-folder.py
+++ create-folder.py
@@ -3,6 +3,8 @@ import glob, os, shutil
 path = os.getcwd()
 print ("The current working directory is %s" % path)
 
-#for file_path in glob.glob(os.path.join(path,"*.*")):
-#    new_dir = file_path.rsplit('.', 1)[0]
- 
+for file_path in glob.glob(os.path.join(path,"*.tex")):
+    new_dir = file_path.rsplit('.', 1)[0]
+    os.mkdir(os.path.join(path, new_dir))
+    shutil.move(file_path, os.path.join(new_dir, os.path.basename(file_path)))
+    print(new_dir,"was created")
