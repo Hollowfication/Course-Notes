032fb9918acec3783377d063a2252c68c4cfbf07
Nigel Dias <d_nigel_@hotmail.com>
2021-12-08 19:41:49 -0500

    Add files via upload

---
+++ create-folder.py
@@ -0,0 +1,8 @@
+import glob, os, shutil
+
+path = os.getcwd()
+print ("The current working directory is %s" % path)
+
+#for file_path in glob.glob(os.path.join(path,"*.*")):
+#    new_dir = file_path.rsplit('.', 1)[0]
+ 
